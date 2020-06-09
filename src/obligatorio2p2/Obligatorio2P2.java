89fc4b63c8b8fb5abff8cf64c53de9e17c78fc41
Ivan Kaitazoff <ivankaitazoff@gmail.com>
2020-06-09 09:25:08 -0300

    Initial commit

---
+++ src/obligatorio2p2/Obligatorio2P2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package obligatorio2p2;
+
+/**
+ *
+ * @author ivan
+ */
+public class Obligatorio2P2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
